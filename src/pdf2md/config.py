4b768f8bba58244ecaa5f6acabc4ea333fa36405
Vrishant <vrishant2bhalla@gmail.com>
2026-08-29 12:37:58 +0530

    change: Height reshape

--- src/pdf2md/config.py
+++ src/pdf2md/config.py
@@ -65,10 +65,10 @@ class PipelineConfig:
     # Large-table splitting (OOM mitigation & multi-column tracking)
     # Dense financial tables overwhelm Qwen2-VL 2B even at moderate heights.
     # We split any crop taller than 800px into strips with headers stitched on.
-    table_split_threshold_px: int = 400           # crops taller than this get split
+    table_split_threshold_px: int = 800           # crops taller than this get split
     qwen_max_pixels: int = 2_000_000
     qwen_min_pixels: int = 3_136
-    table_split_band_pt: int = 200
+    table_split_band_pt: int = 400
     table_split_overlap_pt: int = 20
 
     ocr_fallback_enabled: bool = True
